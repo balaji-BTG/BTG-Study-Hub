1b781d89415b2c17e5e59165a555cd6965e932ad
balaji-BTG <baluy87900@gmail.com>
2025-02-25 16:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
- document.getElementById("loginForm").addEventListener("submit", function(event) {
-            event.preventDefault(); // Prevent form from submitting traditionally
-            let username = document.getElementById("username").value;
-            let password = document.getElementById("password").value;
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from submitting traditionally
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-            if (username && password) {
-                // Redirect to another page (e.g., dashboard.html)
-                window.location.href = "stocks.html"; // Modify this to your actual next page
-            } else {
-                alert("Please enter both username and password.");
-            }
-        });
+    if (username && password) {
+        // Redirect to another page (e.g., stocks.html)
+        window.location.href = "stocks.html"; // Modify this to your actual next page
+    } else {
+        alert("Please enter both username and password.");
+    }
+});
